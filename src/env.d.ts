86c599a6ed4cf05fa7b410292158bc925edc9d28
David Maxwell <david@maxwell.uk.net>
2021-11-30 11:37:18 +0000

    urql client instantiated, .env variables used

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,19 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import { DefineComponent } from 'vue'
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+	readonly VITE_APP_TITLE: string
+	readonly VITE_GRAPHQL_ENDPOINT: string
+	readonly VITE_SECRET: string
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import urql, { createClient } from '@urql/vue'
 
-createApp(App).mount('#app')
+const client = createClient({
+	url: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+	fetchOptions: () => {
+		// const token = getToken();
+		return {
+			headers: {
+				'x-hasura-admin-secret': import.meta.env.VITE_SECRET,
+			},
+		}
+	},
+})
+
+const app = createApp(App)
+app.use(urql, client)
+app.mount('#app')
